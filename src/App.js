775c68bd337038d464ba66e6e4e77e7d3a50cf6f
ahmedmami123 <ahmedmami940@gmail.com>
2022-06-07 13:14:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [person, setPerson] = useState({
+    fullName: "ahmed",
+    Bio: "ahmed_mami95",
+    imageSrc: "Fichier 1-8.png",
+    profession: "Responsable de communication et marketing",
+  });
+  const [show, SetShow] = useState(false);
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, !show);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => SetShow(!show)}>show profile</button>
+      {show ? (
+        <div className="card">
+          <img src={person.imageSrc} width="300px" />
+          <h1>{person.fullName}</h1>
+          <h1>{person.Bio}</h1>
+          <h1>{person.profession}</h1>
+        </div>
+      ) : null}
+    
+   <h1>{seconds}</h1>
+ 
+
     </div>
+    
   );
+
 }
 
 export default App;
